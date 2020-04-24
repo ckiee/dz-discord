aac850d12a41223ec6de00e473a5efe94fa6a8fa
Ron B <me@ronthecookie.me>
2020-04-24 14:14:16 +0300

    comment out eval cause eval = dangerous

--- src/modules/etc.ts
+++ src/modules/etc.ts
@@ -21,31 +21,31 @@ export default class EtcModule extends Module {
 	ready() {
 		this.client.user?.setPresence({ activity: { name: "d!help" } });
 	}
-	@command({ single: true })
-	async eval(msg: Message, js: string) {
-		if (msg.author.id !== "142244934139904000") throw new Error("not ron");
-		console.log("EVAL", js);
-		try {
-			let result = eval(js);
-			if (result instanceof Promise) result = await result;
-			if (typeof result != `string`) result = inspect(result);
-			if (result.length > 1990)
-				return await msg.channel.send(
-					`Message is over the discord message limit.`
-				);
-			await msg.channel.send(
-				"```js\n" +
-					result.split(this.client.token).join("[TOKEN]") +
-					"\n```"
-			);
-		} catch (error) {
-			msg.reply(
-				"error! " +
-					(error || "")
-						.toString()
-						.split(this.client.token)
-						.join("[TOKEN]")
-			);
-		}
-	}
+	// @command({ single: true })
+	// async eval(msg: Message, js: string) {
+	// 	if (msg.author.id !== "142244934139904000") throw new Error("not ron");
+	// 	console.log("EVAL", js);
+	// 	try {
+	// 		let result = eval(js);
+	// 		if (result instanceof Promise) result = await result;
+	// 		if (typeof result != `string`) result = inspect(result);
+	// 		if (result.length > 1990)
+	// 			return await msg.channel.send(
+	// 				`Message is over the discord message limit.`
+	// 			);
+	// 		await msg.channel.send(
+	// 			"```js\n" +
+	// 				result.split(this.client.token).join("[TOKEN]") +
+	// 				"\n```"
+	// 		);
+	// 	} catch (error) {
+	// 		msg.reply(
+	// 			"error! " +
+	// 				(error || "")
+	// 					.toString()
+	// 					.split(this.client.token)
+	// 					.join("[TOKEN]")
+	// 		);
+	// 	}
+	// }
 }
