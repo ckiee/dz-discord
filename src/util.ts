af6ec8003c2be395481febbc4b696cacfc581b42
Ron B <me@ronthecookie.me>
2020-06-10 01:25:14 +0300

    fix bug, uninvert eq check

--- src/util.ts
+++ src/util.ts
@@ -3,14 +3,11 @@ import { isdChannel, directors } from "./env";
 
 export async function collectMessage(msg: Message) {
 	const res = (
-		await msg.channel.awaitMessages(
-			(nm) => nm.author.id !== msg.author.id,
-			{
-				max: 1,
-				time: 1000 * 60 * 2,
-				errors: ["time"],
-			}
-		)
+		await msg.channel.awaitMessages((nm) => nm.author.id == msg.author.id, {
+			max: 1,
+			time: 1000 * 60 * 2,
+			errors: ["time"],
+		})
 	).first();
 	if (!res) throw new Error("couldnt collect message");
 	return res;
